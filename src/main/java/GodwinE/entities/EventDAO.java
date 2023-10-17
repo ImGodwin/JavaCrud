5737e213db10daa99c5db3fbbf98d86548a9a5e2
Godwin <godwinmail6@gmail.com>
2023-10-17 16:15:34 +0200

    Dao has been created. Delete and the various methods added

---
+++ src/main/java/GodwinE/entities/EventDAO.java
@@ -0,0 +1,46 @@
+package GodwinE.entities;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import java.util.UUID;
+
+public class EventDAO {
+
+private final EntityManager entityManagerFactory;
+
+    public EventDAO(EntityManager entityManager)
+    {
+        this.entityManagerFactory = entityManager;
+    }
+
+    public void save(Event event)
+    {
+        EntityTransaction transaction = entityManagerFactory.getTransaction();
+        transaction.begin();
+
+        transaction.commit();
+        System.out.println("New event saved created successfully");
+    }
+
+    public Event getById(UUID id)
+    {
+        return entityManagerFactory.find(Event.class, id);
+    }
+
+    public void delete(UUID id)
+    {
+        Event found = entityManagerFactory.find(Event.class, id);
+
+        if(found != null){
+            EntityTransaction transaction = entityManagerFactory.getTransaction();
+
+            transaction.begin();
+            entityManagerFactory.remove(found);
+
+            transaction.commit();
+            System.out.println("The event was deleted successfully");
+        }else{
+            System.out.println(id + " not found");
+        }
+    }
+}
